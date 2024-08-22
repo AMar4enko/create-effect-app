2ebc1641967485e83ffec428c936f7885c9535ba
Aleks Marchenko <amar4enko@gmail.com>
2024-08-22 10:19:27 +0200

    fix: CI/CD

--- .github/build-action/entrypoint.sh
+++ .github/build-action/entrypoint.sh
@@ -4,4 +4,4 @@ tsc -p tsconfig.json
 cp -r template build/template
 cd build
 pnpm i --ignore-scripts
-pnpm publish --access=public --no-git-checks build --ignore-scripts
+pnpm publish  --ignore-scripts --access=public --no-git-checks build
